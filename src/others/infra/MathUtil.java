db56991b214ee2aaec4b9c5b56fe7fbac35bc5d8
AbirFFA <42368238+AbirFFA@users.noreply.github.com>
2018-08-14 15:53:56 +0600

    Add files via upload

---
+++ src/others/infra/MathUtil.java
@@ -0,0 +1,18 @@
+package others.infra;
+
+/**
+ * MathUtil class.
+ *
+ * @author Leonardo Ono (ono.leo@gmail.com)
+ */
+public class MathUtil {
+
+    public static int clamp(int v, int min, int max) {
+        return Math.min(Math.max(v, min), max);
+    }
+
+    public static double clamp(double v, double min, double max) {
+        return Math.min(Math.max(v, min), max);
+    }
+
+}
